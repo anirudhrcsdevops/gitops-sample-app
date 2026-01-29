03e2c5cd508c175757146cebaa114942dcb59f8e
Anirudh Coppisetti <anirudh.coppisetti@rapyder.com>
2026-01-29 16:04:12 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Github Actions using eks🚀');
+app.get("/", (req, res) => {
+  res.send("Hello from GitLab CI → ECS 🚀");
 });
 
 app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log("App running on port 3000");
 });
