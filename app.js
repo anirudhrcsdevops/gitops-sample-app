5bcccc76365f438051aedfd68073ebbc3bd2c1a1
Anirudh Coppisetti <anirudh.coppisetti@rapyder.com>
2026-01-29 08:37:06 +0530

    githubactions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Github Actions 🚀');
+  res.send('Hello from Github Actions test🚀');
 });
 
 app.listen(3000, () => {
